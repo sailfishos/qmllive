cd0f55a2b6d1918bd0d6f2be4761c012dbf6de72
Martin Kampas <martin.kampas@jolla.com>
2018-10-25 07:49:09 +0200

    Extract Overlay from OverlayUrlInterceptor
    Change-Id: I3b7fd97a2e95f0b4ff707217e3b2134294771ea3

--- src/livenodeengine.h
+++ src/livenodeengine.h
@@ -40,6 +40,7 @@
 
 class LiveRuntime;
 class ContentPluginFactory;
+class Overlay;
 class OverlayUrlInterceptor;
 
 class QMLLIVESHARED_EXPORT LiveNodeEngine : public QObject
@@ -120,8 +121,6 @@ private:
     void checkQmlFeatures();
     QUrl errorScreenUrl() const;
     QUrl queryDocumentViewer(const QUrl& url);
-    void initOverlay();
-    void destroyOverlay();
 
 private:
     int m_xOffset;
@@ -136,6 +135,7 @@ private:
     QDir m_workspace;
     WorkspaceOptions m_workspaceOptions;
     QPointer<OverlayUrlInterceptor> m_overlayUrlInterceptor;
+    QPointer<Overlay> m_overlay;
     QTimer *m_delayReload;
 
     ContentPluginFactory* m_pluginFactory;
